001569ab59d6fcbf1c0ee4448b6f146efe2c0753
João Felipe Alles <joaoalles123@gmail.com>
2023-06-05 12:56:14 -0300

    V2

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const form = document.querySelector('form');
+  const form = document.getElementById('contact-form');
+  const arquivoInput = document.getElementById('anexo');
 
   form.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -13,23 +14,28 @@ document.addEventListener('DOMContentLoaded', function() {
     } else if (!validateEmail(email)) {
       alert('Por favor, insira um endereço de e-mail válido.');
     } else if (!validateTelefone(telefone)) {
-      alert('Por favor, insira um número de telefone válido (formato: XXXX-XXXX).');
+      alert('Por favor, insira um número de telefone válido (formato: (XX)XXXXX-XXXX).');
+    } else if (arquivoInput.files.length === 0) {
+      alert('Por favor, anexe um arquivo antes de enviar o formulário.');
     } else {
-      alert('Formulário enviado com sucesso!');
       form.reset();
-      window.location.href = './sucesso.html'; // Redireciona para a página "sucesso.html"
+      window.location.href = './sucesso.html';
     }
   });
 
   function validateEmail(email) {
-    // Expressão regular para validar o formato do e-mail
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   }
 
   function validateTelefone(telefone) {
-    // Expressão regular para validar o formato do telefone (XXXX-XXXX)
-    const telefoneRegex = /^\d{4}-\d{4}$/;
+    const telefoneRegex = /^\(\d{2}\)\d{5}-\d{4}$/;
     return telefoneRegex.test(telefone);
   }
+  
+  arquivoInput.addEventListener('change', function() {
+    if (arquivoInput.files.length === 0) {
+      alert('Por favor, anexe um arquivo antes de enviar o formulário.');
+    }
+  });
 });
